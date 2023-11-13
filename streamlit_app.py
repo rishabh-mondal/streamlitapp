4b481bb2764b51d264297bd75cd01f11cddf6760
rishabh-mondal <rishabh.mondal19@gmail.com>
2023-11-14 01:17:14 +0530

    Update requirements.txt and streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,222 +1,32 @@
-# Numerical analysis
 import numpy as np
-import pandas as pd
-#visualization
-import altair as alt
-#streamlit platform 
+
+# import torch
+# import hamiltorch
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
 import streamlit as st
-# Model
-from sklearn.linear_model import LinearRegression
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
-from sklearn.metrics import mean_squared_error
+import torch.nn as nn
+import seaborn as sns
 
-alt.themes.enable('dark')
-st.markdown("""
-<style>
-body {
-    color: #333;
-    font-family: 'Helvetica', 'Arial', sans-serif;
-}
+# sns.set_style("darkgrid")
+import torch.nn.functional as F
 
-header {
-    background-color: #333;
-    padding: 20px;
-    text-align: center;
-}
+st.set_option("deprecation.showPyplotGlobalUse", False)
 
-h1, h2 {
-    color: #FF6600;
-}
+# load the data
 
-.container {
-    max-width: 800px;
-    margin: auto;
-    padding: 20px;
-}
+X, y = make_moons(n_samples=1000, noise=0.1, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=42
+)
 
-.stButton button {
-    background-color: #FF6600;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 16px;
-}
-
-.stButton button:hover {
-    background-color: #E64A00;
-}
-
-.stCheckbox label {
-    font-size: 18px;
-    color: #666;
-}
-
-.stHeader {
-    background-color: #FF6600;
-    color: #fff;
-    padding: 10px;
-    text-align: center;
-    border-radius: 5px;
-    margin-top: 20px;
-}
-
-.stDataFrame {
-    background-color: #fff;
-    color: #333;
-}
-
-.stAltairChart {
-    margin-top: 20px;
-}
-</style>
-""", unsafe_allow_html=True)
-
-def generate_data_linear(seed, slope, intercept, num_data_points):
-    rng = np.random.RandomState(seed)
-    x = 10 * rng.rand(num_data_points) + 0.3 * rng.randn(num_data_points)  # Adding noise to 'x'
-    y = slope * x + intercept + rng.randn(num_data_points)
-    return x, y
-
-def generate_data_sine(seed, slope, intercept, num_data_points):
-    rng = np.random.RandomState(seed)
-    x = 10 * rng.rand(num_data_points) + rng.randn(num_data_points)  # Adding noise to 'x'
-    y = np.sin(x) + 0.3 * rng.randn(num_data_points)
-    return x, y
-
-def generate_data_exponential(seed, slope, intercept, num_data_points):
-    rng = np.random.RandomState(seed)
-    x = 10 * rng.rand(num_data_points) + rng.randn(num_data_points)  # Adding noise to 'x'
-    y = np.exp(x) + 0.3 * rng.randn(num_data_points)
-    return x, y
-
-def generate_data_polynomial(seed, slope, intercept, num_data_points):
-    rng = np.random.RandomState(seed)
-    x = 10 * rng.rand(num_data_points) + rng.randn(num_data_points)  # Adding noise to 'x'
-    y = slope * x**2 + intercept + rng.randn(num_data_points)
-    return x, y
-
-def generate_data_gaussian_process(seed, slope, intercept, num_data_points):
-    rng = np.random.RandomState(seed)
-    x = 10 * rng.rand(num_data_points)
-    y = slope * x + intercept + rng.randn(num_data_points)  # Adding noise to 'y'
-    
-    # Use Gaussian Process Regressor to generate data
-    kernel = C() * RBF()
-    gp = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, random_state=seed)
-    gp.fit(x[:, np.newaxis], y)
-    yfit = gp.predict(np.linspace(0, 10, num_data_points)[:, np.newaxis])
-    return x,y
-
-# Slider Section
-seed = st.slider("Random seed", 1, 50, 10)
-slope = st.slider("Slope", -10.0, 10.0, 2.0, 0.1)
-intercept = st.slider("Intercept", -10.0, 10.0, -5.0, 0.1)
-num_data_points = st.slider("Number of Data Points", 10, 5000, 50)
-col1, col2 = st.columns(2)
-
-# Checkbox Section for selecting data generation functions
-with col1:
-    st.markdown("### Data Generation Functions")
-    generate_linear = st.checkbox("Linear Function")
-    generate_sine = st.checkbox("Sine Function")
-    generate_exponential = st.checkbox("Exponential Function")
-    generate_polynomial = st.checkbox("Polynomial Function")
-    generate_gaussian_process = st.checkbox("Gaussian Process Function")
-
-# Section for Regression Line Function Selection
-with col2:
-    st.markdown("### Regression Line Function")
-    poly_regression = st.checkbox("Polynomial Regression (Degree 7)")
-    rbf_regression = st.checkbox("RBF Regression (Gaussian Process)")
-
-if poly_regression:
-    poly_degree = st.slider("Polynomial Degree", 1, 20, 7, key='poly_degree')
+# visualize the data
+st.subheader("Make Moons Dataset")
+scatter_plot = sns.scatterplot(x=X[:, 0], y=X[:, 1], hue=y, palette="Set1")
+scatter_plot.set_title("Make Moons Dataset")
+scatter_plot.set(xlabel="X1", ylabel="X2")
+st.pyplot()
 
 
-# Generate data based on the selected functions
-x, y = [], []
-
-if generate_linear:
-    x_temp, y_temp = generate_data_linear(seed, slope, intercept, num_data_points)
-    x.extend(x_temp)
-    y.extend(y_temp)
-
-if generate_sine:
-    x_temp, y_temp = generate_data_sine(seed, slope, intercept, num_data_points)
-    x.extend(x_temp)
-    y.extend(y_temp)
-
-if generate_exponential:
-    x_temp, y_temp = generate_data_exponential(seed, slope, intercept, num_data_points)
-    x.extend(x_temp)
-    y.extend(y_temp)
-
-if generate_polynomial:
-    x_temp, y_temp = generate_data_polynomial(seed, slope, intercept, num_data_points)
-    x.extend(x_temp)
-    y.extend(y_temp)
-
-if generate_gaussian_process:
-    x_temp, y_temp = generate_data_gaussian_process(seed, slope, intercept, num_data_points)
-    x.extend(x_temp)
-    y.extend(y_temp)
-
-if len(x) > 0:
-    # Create DataFrame for the scatter plot
-    data = pd.DataFrame({'x': x, 'y': y})
-
-    if poly_regression:
-        model = make_pipeline(PolynomialFeatures(degree=7), LinearRegression(fit_intercept=True))
-    elif rbf_regression:
-        kernel = C() * RBF()
-        model = GaussianProcessRegressor(kernel=kernel, n_restarts_optimizer=10, random_state=seed)
-    else:
-        model = LinearRegression(fit_intercept=True)
-    model.fit(np.array(x)[:, np.newaxis], y)
-
-    # Generate data points for regression line
-    xfit = np.linspace(0, 10, 1000)
-    yfit = model.predict(xfit[:, np.newaxis])
-
-    # Scatter plot using Altair
-    scatter_plot = alt.Chart(data).mark_circle(size=60, color='steelblue').encode(
-        x='x',
-        y='y',
-        tooltip=['x', 'y']
-    ).interactive()
-
-    # Line chart for the regression line using Altair
-    regression_line_chart = alt.Chart(pd.DataFrame({'x': xfit, 'y': yfit})).mark_line(
-        color='orange', strokeWidth=2
-    ).encode(
-        x='x',
-        y='y'
-    )
-
-    # Combine the scatter plot and regression line chart using Altair
-    combined_chart = (scatter_plot + regression_line_chart).properties(
-        width=600,
-        height=400
-    )
-
-    # Show the combined chart
-    st.altair_chart(combined_chart)
-     # Make predictions on the data
-    y_pred = model.predict(np.array(x)[:, np.newaxis])
-
-    # Calculate Mean Squared Error (MSE)
-    mse = mean_squared_error(y, y_pred)
-
-    # Calculate Root Mean Squared Error (RMSE)
-    rmse = np.sqrt(mse)
-
-    # Display MSE and RMSE in a box
-    st.info(f"MSE: {mse:.4f}")
-    st.info(f"RMSE: {rmse:.4f}")   
-else:
-    st.write("Please select at least one data generation function.")
+# Classification on MakeMoons dataset: a GP from GPy
